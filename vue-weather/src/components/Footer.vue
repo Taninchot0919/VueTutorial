bbd72b853a7c2a6a53c3a70e8002677777620fe7
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-01-24 13:24:33 +0700

    [FIRST ADD] Vue Weather Website
    Added Vue Weather Website

---
+++ vue-weather/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="Footer" class="Footer">Coding by Taninchot Phuwaloetthiwat</div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+};
+</script>
+
+<style scoped>
+.Footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: rgb(78, 78, 78);
+  color: white;
+  text-align: center;
+}
+</style>
