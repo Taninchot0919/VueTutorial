313f0ec9882ef01b06837b9d9f6c0a1724813c52
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-01-24 23:41:08 +0700

    [FIRST ADD] Convert Project
    Convert First HTML Project to VueJS

---
+++ first-project-convert/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+* {
+  background-image: url("./assets/background.jpg");
+}
+</style>
---
+++ first-project-convert/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="header">
+      <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+};
+</script>
+
+<style>
+</style>
